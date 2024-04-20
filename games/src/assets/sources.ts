cc235c6e749c1d91574ce9620637df85feb38300
Lorenzo Boffa <85372151+LorenzoBoffa@users.noreply.github.com>
2024-04-20 14:54:13 +0200

    Add icons, images and sources references

---
+++ games/src/assets/sources.ts
@@ -0,0 +1,23 @@
+import rockIcon from '@/assets/rock.svg';
+import paperIcon from '@/assets/paper.svg';
+import scissorsIcon from '@/assets/scissors.svg';
+import rockWinIcon from '@/assets/rock-win.svg';
+import paperWinIcon from '@/assets/paper-win.svg';
+import scissorsWinIcon from '@/assets/scissors-win.svg';
+import swordsIcon from '@/assets/swords.svg';
+
+ const iconPaths = {
+  rock: rockIcon,
+  paper: paperIcon,
+  scissors: scissorsIcon,
+};
+
+const winIconPaths = {
+  rock: rockWinIcon,
+  paper: paperWinIcon,
+  scissors: scissorsWinIcon,
+};
+const lostMeme = "https://media.tenor.com/kkGmDDdulUkAAAAj/you-lost-mo%27nique.gif";
+const winMeme = "https://media2.giphy.com/media/eoxomXXVL2S0E/giphy.gif";
+
+export {iconPaths, winIconPaths, swordsIcon, lostMeme, winMeme}
